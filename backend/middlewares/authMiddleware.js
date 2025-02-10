408e78cbcc23b4a4c45e04a61d80c0c171885249
Shivam Patil <shivampatil2110@gmail.com>
2025-02-10 23:01:40 +0530

    changes

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -3,7 +3,14 @@ const jwt = require("jsonwebtoken");
 const jwtConfig = require("../config/jwt");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.token
+  const authHeader =
+    req.headers["authorization"] || req.headers["Authorization"];
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Access denied. No token provided." });
+  }
+
+  const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, jwtConfig.secret);
     req.user = decoded; // Attach the decoded payload (e.g., user ID, isArtist flag) to the request object
